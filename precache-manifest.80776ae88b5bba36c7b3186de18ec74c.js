a7b5ac216999450bb13056b4d21c18caaef388f0
dafunk <759974271@qq.com>
2022-02-23 13:02:58 +0800

    Updates

---
+++ precache-manifest.80776ae88b5bba36c7b3186de18ec74c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb37304d1f9c6eba3726638bab925cf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7376ef7952bf08f07df5",
+    "url": "./static/css/2.d75e0ccb.chunk.css"
+  },
+  {
+    "revision": "acc8cf54a595fb60de36",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "7376ef7952bf08f07df5",
+    "url": "./static/js/2.c1aec7ab.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c1aec7ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756734d438899cd2097f",
+    "url": "./static/js/3.272fb408.chunk.js"
+  },
+  {
+    "revision": "166aff3af6b10327decc",
+    "url": "./static/js/4.32c952db.chunk.js"
+  },
+  {
+    "revision": "acc8cf54a595fb60de36",
+    "url": "./static/js/main.040345af.chunk.js"
+  },
+  {
+    "revision": "714e05a1e7eb09a6fbeb",
+    "url": "./static/js/runtime-main.1113643c.js"
+  }
+]);
